3f35efb6a267f44c95ceead8727e589046b55801
evelynzz <zhao.yue3@northeastern.edu>
2023-04-09 12:46:55 -0700

    retry1

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-//    || 'mongodb://127.0.0.1:27017/tuiter'
+    || 'mongodb://127.0.0.1:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 
 //mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
