c6e6f9f11bccb70387e2f3356e8fc0d5d849699d
evelynzz <zhao.yue3@northeastern.edu>
2023-04-09 11:17:48 -0700

    add schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,6 +3,15 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    image: String,
+    username: String,
+    time: String,
+    replies: Number,
+    retuits: Number,
+    dislikes: Number,
+    disliked: Boolean,
+    handle: String,
+
 }, {collection: 'tuits'});
 
 export default schema;
